884c7a72e748dfb912131a243dab586ec6a8da3d
swxctx <swxctx@gmail.com>
2020-06-16 19:01:38 +0800

    add example

---
+++ example/develop/develop.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/swxctx/malatd"
+)
+
+func main(){
+	// new server
+	srv := malatd.NewServer(malatd.NewSrvConfig())
+
+	// api router
+	srv.Get("/malatd", func(ctx *malatd.Context) {
+		ctx.String(200,"hello malatd")
+	})
+	srv.ListenAndServe()
+}
