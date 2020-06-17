3597ba439c78c49d56ac919444e1a43c29c7d845
swxctx <swxctx@gmail.com>
2020-06-17 19:40:33 +0800

    format fix

--- log.go
+++ log.go
@@ -6,6 +6,6 @@ import (
 
 // runLog
 func runLog(ctx *Context) {
-	xlog.Infof("From %s, %s", ctx.Ctx.RemoteAddr().String(), string(ctx.Ctx.Method()), ctx.Ctx.Request.String())
+	xlog.Infof("From %s, %s", ctx.Ctx.RemoteAddr().String(), ctx.Ctx.Request.String())
 	ctx.Next()
 }
