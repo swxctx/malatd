6de628c3bb74ab664b3e591d0caff03d3c945aa7
swxctx <swxctx@gmail.com>
2021-08-18 19:33:43 +0800

    add 增加公用错误码

---
+++ common.go
@@ -0,0 +1,58 @@
+package td
+
+const (
+	// 公用错误码
+	CodeUnknownError = -1
+	CodeWriteFailed  = 104
+
+	CodeInvalidParameter    = 299
+	CodeBadPacket           = 400
+	CodeUnauthorized        = 401
+	CodeNotFound            = 404
+	CodeInternalServerError = 500
+	CodeBadGateway          = 502
+)
+
+// CodeMessage
+func CodeMessage(rerrCode int) string {
+	switch rerrCode {
+	case CodeBadPacket:
+		return "Bad Packet"
+	case CodeUnauthorized:
+		return "Unauthorized"
+	case CodeWriteFailed:
+		return "Write Failed"
+	case CodeNotFound:
+		return "Not Found"
+	case CodeInternalServerError:
+		return "Internal Server Error"
+	case CodeBadGateway:
+		return "Bad Gateway"
+	case CodeUnknownError:
+		fallthrough
+	case CodeInvalidParameter:
+		return "Invalid Parameter"
+	default:
+		return "Unknown Error"
+	}
+}
+
+var (
+	// 公用错误
+	RerrUnknown          = NewRerror(CodeUnknownError, CodeMessage(CodeUnknownError), "")
+	RerrWriteFailed      = NewRerror(CodeWriteFailed, CodeMessage(CodeWriteFailed), "")
+	RerrInvalidParameter = NewRerror(CodeInvalidParameter, CodeMessage(CodeInvalidParameter), "")
+	RerrBadPacket        = NewRerror(CodeBadPacket, CodeMessage(CodeBadPacket), "")
+	RerrNotFound         = NewRerror(CodeNotFound, CodeMessage(CodeNotFound), "")
+	RerrInternalServer   = NewRerror(CodeInternalServerError, CodeMessage(CodeInternalServerError), "")
+)
+
+const (
+	// 客户端IP
+	RemoteIP = "TD-Remote-IP"
+)
+
+// GetRemoteIP
+func GetRemoteIP() {
+
+}
