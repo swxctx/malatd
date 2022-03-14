3e92911a7313a688fe1b60acda3d641eb45edf97
swxctx <swxctx@gmail.com>
2022-03-14 12:12:35 +0800

    update method file

--- util.go
+++ util.go
@@ -15,3 +15,22 @@ func StringToBytes(s string) []byte {
 func BytesToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
+
+// getReqPath
+func getReqPath(h1, h2 string) string {
+	u := string(h1[len(h1)-1])
+	if u == "/" {
+		u = h1[:len(h1)-1]
+	} else {
+		u = h1
+	}
+
+	u2 := string(h2[0])
+	if u2 == "/" {
+		u += h2
+	} else {
+		u += "/" + h2
+	}
+
+	return u
+}
